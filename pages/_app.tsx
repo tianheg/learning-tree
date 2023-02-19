aefc23d3e2bcabf27511244a983303c806d7c48d
tianheg <yidajiabei@gmail.com>
2023-02-19 08:05:45 +0800

    import style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { OrgaProvider } from '@orgajs/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 const components = {
   a: Link,
